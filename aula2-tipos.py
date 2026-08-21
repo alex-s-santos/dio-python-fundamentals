1e0000b94c4a62f2d3d24b41107b7246502823ae
Alex Santos <alexsantos.alss.13@gmail.com>
2026-08-21 12:27:40 -0300

    sobe aula 2

---
+++ aula2-tipos.py
@@ -0,0 +1,9 @@
+#COMENTÁRIO EM PYTHON COLOCA #
+#número inteiro 
+print(10 + 10);
+#número flutuante
+print(7.5 + 7.5);
+#Booleano
+print(True); #Inicia com letra maiúscula
+#String (texto)
+print("Python");
