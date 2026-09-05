fb27958aae89c4223739978e9cd7fdfff09a6ef4
Alex Santos <alexsantos.alss.13@gmail.com>
2026-09-05 11:49:27 -0300

    aula 4 conversões

---
+++ aula4-conversoes-variaveis.py
@@ -0,0 +1,10 @@
+#usando construtores
+#float(variavel ou valor) = Para converter para float
+#int(variável) = Para converter para inteiro
+#str(variável) = Para converter para variável
+
+#Para ver o tipo da variável type(variável)
+
+#Divisão de inteiros
+print(100 / 2); #com apenas um sinal de divisão retorna flutuante 
+print(100 // 2); #com dois sinais retorna inteiro
